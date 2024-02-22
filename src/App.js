ef5c6039d2636ea553cad194e35cf91004ee0702
BxNoel <110917072+BxNoel@users.noreply.github.com>
2024-02-21 22:01:28 -0500

    final setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>Hello Team: Please Get Working on Your Task ASAP, let me know if you have any questions
+      This should leave you with a blank canvas to get working.</>
     </div>
   );
 }
