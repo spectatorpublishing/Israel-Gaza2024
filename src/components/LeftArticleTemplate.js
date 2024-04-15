040415e794d77a6957092029480e754205a9b9a1
BxNoel <110917072+BxNoel@users.noreply.github.com>
2024-04-14 21:50:43 -0400

    changes

--- src/components/LeftArticleTemplate.js
+++ src/components/LeftArticleTemplate.js
@@ -108,22 +108,6 @@ return (
 </ArticleWrap>
 );
 
-const Image = styled.img`
-  height: 100px;
-  width: 200px;
-`
-
-const LeftArticleTemplate = ({article}) => {
-  return (
-    <div>
-        <p>Left Article</p>
-        <Image src={article.image_url} />
-        <div href={article.article_link} target="_blank">{article.article_title}</div>
-        <div>{article.article_authors}</div>
-        <div>{article.article_date}</div>
-        <div>{article.article_section}</div>
-    </div>
-  );
-};
+}
 
 export default LeftArticleTemplate;
